f001d562dc89fe91f1104efee670246081af0199
Zendo technology <zendotechnology@gmail.com>
2020-06-23 09:19:47 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1:7acec4d8-b436-4475-97c0-3cfb51aa079c', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
